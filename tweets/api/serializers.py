97c97a928c3abf94ad6503afd191db6db1cb69b7
Pandza11 <ivan.pandzic@gmail.com>
2018-07-24 18:25:16 +0200

    Added user profile pages

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,3 +1,4 @@
+from django.utils.timesince import timesince
 from rest_framework import serializers
 
 from accounts.api.serializers import UserDisplaySerializer
@@ -6,10 +7,20 @@ from tweets.models import Tweet
 
 class TweetModelSerializer(serializers.ModelSerializer):
     user = UserDisplaySerializer()
-
+    timesince = serializers.SerializerMethodField()
+    date_display = serializers.SerializerMethodField()
     class Meta:
         model = Tweet
         fields = [
             'user',
-            'content'
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince'
         ]
+
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%b %d %I:%M %p")
+
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 
 from tweets.models import Tweet
+
+from .pagination import StandardResultsPagination
 from .serializers import TweetModelSerializer
 
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
+    pagination_class = StandardResultsPagination
 
     def get_queryset(self):
         return Tweet.objects.all()
